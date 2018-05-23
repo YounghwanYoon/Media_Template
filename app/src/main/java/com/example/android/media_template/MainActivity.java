98d88cc75b759e1d2b1291e59adad99bfc68df78
RayYoon <ray.yoon87@gmail.com>
2018-05-23 15:13:03 -0700

    Single Media Player is completed
    Play,Pause, Resume methods are added and works.
    
    Directorying system is added.
    
    To Do:
    1. Need to update Directory system to show only media files (such as mp3 and mp4).
    2. Need to add functionality to add media file in sequences.
    3. Need to add functionalities to buttons.

--- app/src/main/java/com/example/android/media_template/MainActivity.java
+++ app/src/main/java/com/example/android/media_template/MainActivity.java
@@ -1,42 +1,159 @@
 package com.example.android.media_template;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.media.AudioManager;
-import android.media.Image;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageButton;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageButton mCheck_list_button;
-    ImageButton mPlayerOrPause_button;
-    ImageButton mNext_button;
-    ImageButton mAdd_List_button;
+    Button mCheck_list_button;
+    Button mPlayerOrPause_button;
+    Button mNext_button;
+    Button mAdd_List_button;
+
     MediaPlayer mMediaPlayer;
 
+    SeekBar positionBar;
+    TextView elapseTime;
+    TextView remainTime;
+    int totalDuration;
+    String selectedFile;
+
+    int state;
+    final static int start_state = 0;
+    final static int pause_state = 1;
+    final static int resume_state = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //connecting button
-        mPlayerOrPause_button = findViewById(R.id.play_or_pause_button);
+        state = start_state;
+        mPlayerOrPause_button = (Button) findViewById(R.id.play_or_pause_button);
+        mAdd_List_button = (Button) findViewById(R.id.add_list_button);
 
+        mMediaPlayer = new MediaPlayer();
+        mPlayerOrPause_button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (state){
+                    case start_state:
+                        selectMusic(selectedFile);
+                        Toast.makeText(MainActivity.this, "Playing:" + selectedFile ,Toast.LENGTH_SHORT).show();
 
+                        mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener(){
+                            @Override
+                            public void onPrepared(MediaPlayer mediaPlayer) {
+                                mediaPlayer.start();
+                            }
+                        });
+                        mMediaPlayer.start();
+                        state++;
+                        break;
+                    case pause_state:
+                        mPlayerOrPause_button.setBackgroundResource(R.drawable.ic_play_button_image);
+                        Toast.makeText(MainActivity.this, "Pause!" ,Toast.LENGTH_SHORT).show();
+                        mMediaPlayer.pause();
+                        state ++;
+                        break;
+                    case resume_state:
+                        mPlayerOrPause_button.setBackgroundResource(R.drawable.ic_pause_button_image);
+                        Toast.makeText(MainActivity.this, "Resume!" ,Toast.LENGTH_SHORT).show();
+                        mMediaPlayer.start();
+                        state--;
+                        break;
+                }
+            }
+        });
 
+        mAdd_List_button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reset();
+                Intent returnSelectedFile_Intent = new Intent(MainActivity.this, SourceListActivity.class);
+                startActivityForResult(returnSelectedFile_Intent,0);
+            }
+        });
 
-         mMediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.not_there_jeongyup);
-        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        //mMediaPlayer.setDataSource(getApplicationContext(), myUri);
-        try {
-            mMediaPlayer.prepare();
-        } catch (IOException e) {
-            e.printStackTrace();
+    }
+
+    private void selectMusic(String selected){
+        mPlayerOrPause_button.setBackgroundResource(R.drawable.ic_pause_button_image);
+        if(selectedFile!=null){
+            //mMediaPlayer.reset();
+            Uri myUri = Uri.parse("file://" + selectedFile);
+            if(mMediaPlayer !=null){
+                    mMediaPlayer.reset();
+            }
+            mMediaPlayer = new MediaPlayer();
+            mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            try {
+                mMediaPlayer.setDataSource(getApplicationContext(), myUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Log.i("MainActivity.java", "inside selectMusic is called!");
+            mMediaPlayer.prepareAsync();
         }
-        mMediaPlayer.start();
+        else
+            mMediaPlayer=MediaPlayer.create(MainActivity.this, R.raw.not_there_jeongyup);
+    }
 
+
+    //Result from selecting a file from External SD Driver
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==0){
+            if(resultCode == Activity.RESULT_OK){
+                state = start_state;
+                selectedFile = data.getStringExtra("result");
+            }
+        }
+    }
+    private void reset() {
+        mPlayerOrPause_button.setBackgroundResource(R.drawable.ic_play_button_image);
+        if (mMediaPlayer != null) {
+            mMediaPlayer.pause();
+            mMediaPlayer.reset();
+        }
+    }
+    private void stop(){
+        if(mMediaPlayer!=null){
+            mMediaPlayer.pause();
+            mMediaPlayer.release();
+            mMediaPlayer=null;
+            mPlayerOrPause_button.setBackgroundResource(R.drawable.ic_play_button_image);
+            state = start_state;
+        }
+    }
+    /*
+    When an activity goes onStop status, release and nullify MediaPlayer object to restore memory of the device.
+     */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stop();
+    }
+
+    /*
+    When an activity goes onStop status, release and nullify MediaPlayer object to restore memory of the device.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stop();
     }
 }
---
+++ app/src/main/java/com/example/android/media_template/SourceListActivity.java
@@ -0,0 +1,122 @@
+package com.example.android.media_template;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SourceListActivity extends ListActivity {
+
+    private List<String> itemsInCurrentPath = null;
+    private List<String> currentPath = null;
+    private String root;
+    private TextView myPath;
+    private File rootFile;
+    private File[] files;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listview);
+
+        myPath = (TextView)findViewById(R.id.path);
+        String secStore = System.getenv("SECONDARY_STORAGE");
+
+        //Environment.getExternalStorageDirectory().getPath()
+        rootFile = new File(secStore);
+        getDir(rootFile);
+
+
+    }
+
+    private void getDir(File currentFolder) {
+
+        itemsInCurrentPath = new ArrayList<String>();
+        currentPath = new ArrayList<String>();
+
+        myPath.setText("Location: "+ currentFolder);
+
+        //files now has list of files in the current folder(directory)
+        files = currentFolder.listFiles();
+
+        if(!currentFolder.equals(rootFile))
+        {
+            itemsInCurrentPath.add(currentFolder.getParent());
+            currentPath.add(rootFile.getParent());
+            currentPath.add(currentFolder.getParent());
+        }
+
+        //Add all of files in the current Path/Folder to list ////  if(!files[i].isHidden() )|| files[i].canRead()   // if(files[i].isDirectory())
+        for(int i=0; i < files.length;i++)
+        {
+            {
+                {
+                    itemsInCurrentPath.add(files[i].getParent()+"/"+ files[i].getName()+"/");
+                }
+            }
+        }
+
+        //R.layout.row_each_directory R.id.individual_file,itemsInCurrentPath
+        ArrayAdapter<String> fileList = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, itemsInCurrentPath);
+        setListAdapter(fileList);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        //One of item in the current folder is selected
+        File selected_file = new File(itemsInCurrentPath.get(position));
+
+        if(selected_file.isDirectory())
+        {
+            if(selected_file.canRead())
+                getDir(selected_file);
+            else //Double caution for selecting non-readable file (which was sorted in getDir();
+                Toast.makeText(SourceListActivity.this,"It cannot be read", Toast.LENGTH_SHORT);
+
+        }
+        //Once selected file is mp3 file, then it return to parent activity.
+        else if(selected_file.getPath().endsWith(".mp3") ){
+            Intent returnIntent = getIntent();//new Intent();
+            returnIntent.putExtra("result",selected_file.getPath() );
+            setResult(Activity.RESULT_OK, returnIntent);
+            finish();
+        }
+        else
+            Toast.makeText(SourceListActivity.this,"It is not a directory", Toast.LENGTH_SHORT);
+
+
+        /*        if (file.isDirectory()) {
+            if (file.canRead()) {
+                getDir(new File (currentPath.get(position)));
+            } else {
+                new AlertDialog.Builder(this)
+                        .setIcon(R.drawable.ic_media_startup_image)
+                        .setTitle("[" + file.getName() + "] folder can't be read!")
+                        .setPositiveButton("OK", null)
+                        .show();
+
+            }
+        }
+        else{
+            new AlertDialog.Builder(this)
+                    .setIcon(R.drawable.ic_media_startup_image)
+                    .setTitle("[" + file.getName() + "]")
+                    .setPositiveButton("OK", null)
+                    .show();
+        }*/
+    }
+}
